791d030b66fcc28964b14f1393ea9e1d6867dabf
Lefti90 <55137561+Lefti90@users.noreply.github.com>
2022-07-06 11:14:50 +0300

    2_0 aloitettu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Note from './components/Note'
 
-function App() {
+const App = (props) => {
+  const { notes } = props
+
+  const result = notes.map(note => note.id)
+  console.log(result)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
